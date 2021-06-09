27297a2cad95c888905ffdefccf44e832df875eb
Ashrakt Sami <Ashraktsamy_0@yahoo.com>
2021-06-09 16:30:35 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counters from './components/counters.jsx';
+import NavBar from './components/navBar.jsx';
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handelIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({ counters });
+  };
+  handelReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+
+    });
+    this.setState({ counters });
+  };
+  handelDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className="container-fluid">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handelReset}
+            onIncrement={this.handelIncrement}
+            onDelete={this.handelDelete} />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
